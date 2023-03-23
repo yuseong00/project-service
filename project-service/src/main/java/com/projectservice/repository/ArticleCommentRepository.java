17460b7d3c9c263250ab6b00b5ed3db898945512
yuseong00 <114868050+yuseong00@users.noreply.github.com>
2023-03-23 18:22:03 +0900

    [feat] data rest 설정 및 적용

--- project-service/src/main/java/com/projectservice/repository/ArticleCommentRepository.java
+++ project-service/src/main/java/com/projectservice/repository/ArticleCommentRepository.java
@@ -2,6 +2,9 @@ package com.projectservice.repository;
 
 import com.projectservice.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long> {
 }
--- project-service/src/main/java/com/projectservice/repository/ArticleRepository.java
+++ project-service/src/main/java/com/projectservice/repository/ArticleRepository.java
@@ -3,6 +3,9 @@ package com.projectservice.repository;
 import com.projectservice.domain.Article;
 import com.projectservice.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article,Long> {
 }
