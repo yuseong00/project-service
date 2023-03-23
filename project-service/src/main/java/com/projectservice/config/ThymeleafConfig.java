f21ad3ced83d44290caef12982f13c647b480e62
yuseong00 <114868050+yuseong00@users.noreply.github.com>
2023-03-23 21:15:05 +0900

    [feat]게시판 ,게시글 페이지에 부트스트랩 및 예제 코드 적용

--- project-service/src/main/java/com/projectservice/config/ThymeleafConfig.java
+++ project-service/src/main/java/com/projectservice/config/ThymeleafConfig.java
@@ -8,30 +8,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 
-
+@Configuration
 public class ThymeleafConfig {
 
-        @Bean
-        public SpringResourceTemplateResolver thymeleafTemplateResolver(
-                SpringResourceTemplateResolver defaultTemplateResolver,
-                Thymeleaf3Properties thymeleaf3Properties
-        ) {
-            defaultTemplateResolver.setUseDecoupledLogic(thymeleaf3Properties.isDecoupledLogic());
+    @Bean
+    public SpringResourceTemplateResolver thymeleafTemplateResolver(
+            SpringResourceTemplateResolver defaultTemplateResolver,
+            Thymeleaf3Properties thymeleaf3Properties
+    ) {
+        defaultTemplateResolver.setUseDecoupledLogic(thymeleaf3Properties.isDecoupledLogic());
 
-            return defaultTemplateResolver;
-        }
-
-
-        @RequiredArgsConstructor
-        @Getter
-        @ConstructorBinding
-        @ConfigurationProperties("spring.thymeleaf3")
-        public static class Thymeleaf3Properties {
-            /**
-             * Use Thymeleaf 3 Decoupled Logic
-             */
-            private final boolean decoupledLogic;
-        }
-
+        return defaultTemplateResolver;
     }
 
+
+    @RequiredArgsConstructor
+    @Getter
+    @ConstructorBinding
+    @ConfigurationProperties("spring.thymeleaf3")
+    public static class Thymeleaf3Properties {
+        /**
+         * Use Thymeleaf 3 Decoupled Logic
+         */
+        private final boolean decoupledLogic;
+    }
+
+}
