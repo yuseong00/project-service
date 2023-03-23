bffbac19cf447217f08fac41ff596709bdb74292
yuseong00 <114868050+yuseong00@users.noreply.github.com>
2023-03-23 15:56:00 +0900

    #2 도메인 클래스 설계(게시글,댓글)

---
+++ project-service/src/main/java/com/projectservice/domain/Article.java
@@ -0,0 +1,17 @@
+package com.projectservice.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title;   //제목
+    private String content; //본문
+    private String hashtag; //해시태그
+
+    private LocalDateTime createdAt;  //생성일시
+    private String createBy;       //생성자
+    private LocalDateTime modifiedAt;   //수정일시
+    private String modifiedBy;          //수정자
+
+}
